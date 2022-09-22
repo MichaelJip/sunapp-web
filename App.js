603820edd75f60ae3c9aeda8f37126f267aa0989
Michael <92431155+MichaelJip@users.noreply.github.com>
2022-09-22 15:04:09 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-function unduh() {
-  const element = document.getElementById("UNDUH");
-  if (navigator.userAgent.match(/iPhone/i)) {
-    location.replace(
-      "https://apps.apple.com/id/app/sun-education-group/id1446868544?l=id"
-    );
-  } else if (navigator.userAgent.match(/Android/i)) {
-    location.replace(
-      "https://play.google.com/store/apps/details?id=com.sunedu.app&hl=en&gl=US"
-    );
+function navigate() {
+  if (/Mobi|Android/i.test(navigator.userAgent)) {
+    window.location.href =
+      "https://play.google.com/store/apps/details?id=com.sunedu.app&hl=en&gl=US";
+  }
+  if (/Mobi|iPad|iPhone|iPod/i.test(navigator.userAgent)) {
+    window.location.href =
+      "https://apps.apple.com/id/app/sun-education-group/id1446868544?l=id";
   }
 }
